b7341214c963ea3678e2cc68701a27faf815f51f
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-10 17:13:16 +0000

    The calculator can add any number of numbers together

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,10 @@
 function add(numbers){
-    return 0;
+    const arr = numbers.replace(/\s\s+/g, '').split(',').map(x => parseInt(x));
+    const reducer = (acc, curr) => acc + curr;
+    if(isNaN(arr.reduce(reducer))) { return 0; }
+    return arr.reduce(reducer);
 };
 
-// console.log(add(''));
+// console.log(add('1'));
 
 module.exports = add;
