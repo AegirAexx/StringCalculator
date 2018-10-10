9db50545c37875623f434ab14c232eadea1360b8
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-10 21:29:55 +0000

    The calculator will now add a new delimiter if the first two chars are "//" and keep the old ones as well

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,12 +1,18 @@
 function add(numbers){
-    const arr = numbers.replace(/\s\s+/g, '').split(/[,|\n|]/).map(x => parseInt(x)).filter(x => x <= 1000);
+    let delimiter = /[,|\n]/;
+    if(numbers.startsWith('//')){
+        const newDelimiter = numbers.charAt(2);
+        const first = `[,|\\n|`;
+        const end = `]`;
+        numbers = numbers.substring(4);
+        delimiter = new RegExp(first + newDelimiter + end);
+    }
+    const arr = numbers.replace(/\s\s+/g, '').split(delimiter).map(x => parseInt(x)).filter(x => x <= 1000);
     const negatives = arr.filter(x => x < 0);
     if(negatives.length > 0) { return `Negatives are not allowed: ${negatives}`}
     const reducer = (acc, curr) => acc + curr;
     if(isNaN(arr.reduce(reducer))) { return `Your input is invalid.`; }
     return arr.reduce(reducer);
 };
-
-// console.log(add(''));
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,17 +1,21 @@
 const add = require('./stringCalculator');
 
-test("should return fifteen when there is a lot of white space in the string", () => {
+test(`should return fifteen when there is a lot of white space in the string.`, () => {
     expect(add('4      ,5,6')).toBe(15);
 });
 
-test("should return six when a new line is used as a delimiter", () => {
+test(`should return six when a new line is used as a delimiter.`, () => {
     expect(add('1\n2,3')).toBe(6);
 });
 
-test("should not allow negative numbers and then lists them if any", () => {
+test(`should not allow negative numbers and then lists them if any.`, () => {
     expect(add('1, -1, -3')).toBe(`Negatives are not allowed: -1,-3`);
 });
 
-test("should ignore all values greater than 1000", () => {
+test(`should ignore all values greater than 1000.`, () => {
     expect(add('5, 1003, 1000')).toBe(1005);
+});
+
+test(`should add a delimiter if the first two chars are "//".`, () => {
+    expect(add('//;\n5; 10; 30\n100')).toBe(145);
 });
