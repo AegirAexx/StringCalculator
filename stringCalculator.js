0e0a9c359bc60b857b341ecb61ed8a0cd256d991
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-11 00:01:44 +0000

    Refactoring of the code and added detailed comments as documentation.

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,35 +1,45 @@
 function add(numbers){
 
-    // Set up the default delimiter with regular expression.
+    // Set up the default delimiters ("," or "\n") with regular expression.
     let delimiter = /[,|\n]/;
 
-    // Check if the first two chars are "//", create an alternate regular expression and trim the numbers string.
+    // Check if the first two chars are "//".
     if(numbers.startsWith('//')){
+        // Grab the new delimiter.
         const newDelimiter = numbers.charAt(2);
+        // Set up the first part of the new regular expression.
         const first = `[,|\\n|`;
+        // Set up the last part of the new regular expression.
         const end = `]`;
+        // Grab the the rest of the string, the actual numbers.
         numbers = numbers.substring(4);
+        // Last but not the least, create dynamically a new regular expression.
         delimiter = new RegExp(first + newDelimiter + end);
     }
 
-    // Create an array from the string.
-    // > replace all white space with "" >
-    // > split with RegExp delimiter >
-    // > map all the chars to numbers >
-    // > filter all values greater than 1000.
-    const arr = numbers.replace(/\s\s+/g, '').split(delimiter).map(x => parseInt(x)).filter(x => x <= 1000);
+    // Create an array from the numbers string...
+    const arr = numbers
+        // ...replace all white space with an empty space...
+        .replace(/\s\s+/g, '')
+        // ...split the string with a regular expression delimiter...
+        .split(delimiter)
+        // ...parse string numbers to actual numbers...
+        .map(stringNumber => parseInt(stringNumber))
+        // ...filter out all the values greater than 1000.
+        .filter(value => value <= 1000);
     
-    // Push all the negative values to an array so it can be returned.
-    const negatives = arr.filter(x => x < 0);
+    // Check for and push all the negative values to an array so it can be returned.
+    const negatives = arr.filter(potentiallyNegative => potentiallyNegative < 0);
+    // If the array actually has anything in it, the program returns them in a message.
     if(negatives.length > 0) { return `Negatives are not allowed: ${negatives}`}
     
-    // A reducer method to execute on all elements of the array.
-    const reducer = (acc, curr) => acc + curr;
+    // A reducer method to execute on all elements in the array. It's the blueprint to add all the elements together.
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
     
-    // If any of the values in the array are not numbers throw an exception.
+    // If any of the values in the array are not numbers, return a message.
     if(isNaN(arr.reduce(reducer))) { return `Your input is invalid.`; }
     
-    // Finally add all the values together with the reducer method.
+    // Finally add all the values together and return the total sum.
     return arr.reduce(reducer);
 };
 
